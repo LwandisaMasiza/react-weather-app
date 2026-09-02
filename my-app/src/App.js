c47e38b0d8872664d61bc9815efe17652d5c9a77
LwandisaMasiza <166486405+LwandisaMasiza@users.noreply.github.com>
2026-09-02 13:41:39 +0200

    updated the footer and some other app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <h1> Weather App </h1>
-     <footer>
-      This was coded by Lwandisa Masiza it is {""}
-      <a href="https://github.com/LwandisaMasiza/react-weather-app">open source on GitHub</a>
-     </footer>
+      <h1> Weather App </h1>
+      <footer>
+        This was coded by Lwandisa Masiza it is {""}
+        <a href="https://github.com/LwandisaMasiza/react-weather-app">
+          open source on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
